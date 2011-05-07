03d2d4eb478c65bbe3f3a74ea1784b0f67f88f9a
Young Hahn <young@developmentseed.org>
2011-05-06 21:11:30 -0400

    Initial commit of tilelive-mbtiles

---
+++ index.js
@@ -0,0 +1,99 @@
+var MBTiles = require('./lib/mbtiles'),
+    zlib = require('zlib'),
+    Buffer = require('buffer').Buffer,
+    Step = require('step');
+
+module.exports = {
+    MBTiles: MBTiles,
+    pool: function(datasource, options) {
+        return {
+            create: function(callback) {
+                var resource = new MBTiles(
+                    datasource,
+                    options,
+                    function() { callback(resource); }
+                );
+            },
+            destroy: function(resource) {
+                resource.db.close(function() {});
+            }
+        }
+    },
+    serve: function(resource, options, callback) {
+        switch (options.format) {
+        case 'layer.json':
+            Step(
+                function() {
+                    resource.metadata('formatter', this.parallel());
+                    resource.metadata('legend', this.parallel());
+                },
+                function(err, f, l) {
+                    var layer = {};
+                    f && (layer.formatter = f);
+                    l && (layer.legend = l);
+                    callback(null, layer);
+                }
+            );
+            break;
+        case 'grid.json':
+            var grid;
+            Step(
+                function() {
+                    resource.grid(options.x, options.y, options.z, this);
+                },
+                function(err, buf) {
+                    if (err) throw err;
+                    if (!Buffer.isBuffer(buf))
+                        buf = new Buffer(buf, 'binary');
+                    var inflated = zlib.inflate(buf);
+                    this(null,inflated);
+                },
+                function(err, buf) {
+                    if (err) throw err;
+                    grid = buf.toString();
+                    resource.grid_data(options.x, options.y, options.z, this);
+                },
+                function(err, gd) {
+                    if (err) return callback(err);
+                    // Manually append grid data as a string to the grid buffer.
+                    // Ideally we would
+                    //
+                    //     JSON.stringify(_.extend(JSON.parse(grid), { data: gd }))
+                    //
+                    // But calling JSON stringify will escape UTF8 characters of a
+                    // high enough ordinal making the grid data unusable. Instead,
+                    // manipulate the JSON string directly, popping the trailing }
+                    // off and splicing the grid data in at the "data" key.
+                    grid = grid.substr(0, grid.length - 1)
+                        + ', "data":'
+                        + JSON.stringify(gd)
+                        + '}';
+                    callback(err, grid);
+                }
+            );
+            break;
+        default:
+            resource.tile(options.x, options.y, options.z, function(err, image) {
+                callback(err, [image, { 'Content-Type': 'image/png' }]);
+            });
+            break;
+        }
+    },
+    store: function(step, resource, data, callback) {
+        switch (step) {
+        case 'setup':
+            resource.setup(callback);
+            break;
+        case 'tiles':
+            resource.insertTiles(data, callback);
+            break;
+        case 'grids':
+            resource.insertGrids(data, callback);
+            break;
+        case 'finish':
+            callback();
+            break;
+        }
+    }
+};
+
