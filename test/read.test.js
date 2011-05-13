ed9eeaff50bbc5a6a18f0ef7859a49daff9eb904
Konstantin Käfer <github@kkaefer.com>
2011-05-13 17:29:41 -0400

    also fix different schema

--- test/read.test.js
+++ test/read.test.js
@@ -5,7 +5,8 @@ var MBTiles = require('..').MBTiles;
 var fixtures = {
     plain_1: __dirname + '/fixtures/plain_1.mbtiles',
     plain_2: __dirname + '/fixtures/plain_2.mbtiles',
-    plain_3: __dirname + '/fixtures/plain_3.mbtiles'
+    plain_3: __dirname + '/fixtures/plain_3.mbtiles',
+    plain_4: __dirname + '/fixtures/plain_4.mbtiles'
 };
 
 exports['get metadata'] = function(beforeExit) {
@@ -79,6 +80,75 @@ exports['get grids'] = function(beforeExit) {
     };
 
     var mbtiles = new MBTiles(fixtures.plain_2);
+    fs.readdirSync(__dirname + '/fixtures/grids/').forEach(function(file) {
+        var coords = file.match(/^plain_2_(\d+)_(\d+)_(\d+).json$/);
+        if (coords) {
+            mbtiles.grid(coords[1] | 0, coords[2] | 0, coords[3] | 0, function(err, grid) {
+                if (err) throw err;
+                assert.deepEqual(JSON.stringify(grid), fs.readFileSync(__dirname + '/fixtures/grids/' + file, 'utf8'));
+                status.success++;
+            });
+        }
+    });
+
+    mbtiles.grid(1, 0, 0, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(0, 0, -1, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(0, -1, 0, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(1, 8, 3, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(-3, 0, 2, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(2, 3, 18, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(0, 0, 4, yieldsError(status, 'error', 'Grid does not exist'));
+
+    mbtiles.grid(3, 8, 4, yieldsError(status, 'error', 'Grid is invalid'));
+    mbtiles.grid(4, 8, 4, yieldsError(status, 'error', 'Grid is invalid'));
+    mbtiles.grid(5, 8, 4, yieldsError(status, 'error', 'Grid is invalid'));
+    mbtiles.grid(13, 4, 4, yieldsError(status, 'error', 'Grid is invalid'));
+    mbtiles.grid(0, 14, 4, yieldsError(status, 'error', 'Grid is invalid'));
+    mbtiles.grid(0, 7, 3, yieldsError(status, 'error', 'Grid is invalid'));
+    mbtiles.grid(6, 2, 3, yieldsError(status, 'error', 'Grid is invalid'));
+
+    beforeExit(function() {
+        assert.equal(status.success, 50);
+        assert.equal(status.error, 14);
+    });
+};
+
+
+exports['get grids from file without interaction'] = function(beforeExit) {
+    var status = {
+        success: 0,
+        error: 0
+    };
+
+    var mbtiles = new MBTiles(fixtures.plain_1);
+    mbtiles.grid(1, 0, 0, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(0, 0, -1, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(0, -1, 0, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(1, 8, 3, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(-3, 0, 2, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(2, 3, 18, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(0, 0, 4, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(3, 8, 4, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(4, 8, 4, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(5, 8, 4, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(13, 4, 4, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(0, 14, 4, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(0, 7, 3, yieldsError(status, 'error', 'Grid does not exist'));
+    mbtiles.grid(6, 2, 3, yieldsError(status, 'error', 'Grid does not exist'));
+
+    beforeExit(function() {
+        assert.equal(status.success, 0);
+        assert.equal(status.error, 14);
+    });
+};
+
+exports['get grids with different schema'] = function(beforeExit) {
+    var status = {
+        success: 0,
+        error: 0
+    };
+
+    var mbtiles = new MBTiles(fixtures.plain_4);
     fs.readdirSync(__dirname + '/fixtures/grids/').forEach(function(file) {
         var coords = file.match(/^plain_2_(\d+)_(\d+)_(\d+).json$/);
         if (coords) {
