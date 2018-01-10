25804e55d0fa37f2c76ee6db7e84889521435ecb
jingsam <jing-sam@qq.com>
2018-01-10 15:09:15 +0800

    add mode test

---
+++ test/mode.test.js
@@ -0,0 +1,32 @@
+require('sqlite3').verbose();
+
+var fs = require('fs');
+var MBTiles = require('..');
+var tape = require('tape');
+
+var non_existent = __dirname + '/fixtures/non_existent.mbtiles';
+
+try { fs.unlinkSync(non_existent); } catch (err) {}
+
+tape('Open with ro mode', function(assert) {
+  new MBTiles(non_existent + '?mode=ro', (err, mbtiles) => {
+    assert.ok(err);
+    assert.ok(err.message.match(/SQLITE_CANTOPEN: unable to open database file/));
+    assert.end();
+  })
+})
+
+tape('Open with rw mode', function(assert) {
+  new MBTiles(non_existent + '?mode=rw', (err, mbtiles) => {
+    assert.ok(err);
+    assert.ok(err.message.match(/SQLITE_CANTOPEN: unable to open database file/));
+    assert.end();
+  })
+})
+
+tape('Open with rwc mode', function(assert) {
+  new MBTiles(non_existent + '?mode=rwc', (err, mbtiles) => {
+    assert.notOk(err);
+    assert.end();
+  })
+})
